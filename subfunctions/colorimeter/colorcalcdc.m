fa5e16d5c713f951db7a909b3ebd27636211704b
ban.hiroshi <ban.hiroshi@gmail.com>
2015-06-26 09:27:14 +0900

    Update colorcalcdc.m so that it can handle error during the initialization procedure.
    Signed-off-by: ban.hiroshi <ban.hiroshi@gmail.com>

--- subfunctions/colorimeter/colorcalcdc.m
+++ subfunctions/colorimeter/colorcalcdc.m
@@ -2,7 +2,7 @@ classdef colorcalcdc
   % a class to manipulate Cambridge Research Systems ColorCal from MATLAB through a virtual port (CDC) connection
   %
   % Created    : "2015-06-25 16:49:54 ban"
-  % Last Update: "2015-06-25 21:32:07 ban"
+  % Last Update: "2015-06-26 09:26:13 ban"
   %
   % [example]
   % >> cc=colorcal;
@@ -108,105 +108,111 @@ classdef colorcalcdc
         % do nothing
       else
 
-        % ========================================
-        % initialization step 1: run zero-level calibration
-        % ========================================
+        try
 
-        calibrate_ok=0;
-        while ~calibrate_ok
+          % ========================================
+          % initialization step 1: run zero-level calibration
+          % ========================================
 
-          % Commands are passed to the ColorCAL II as though they were being written to a text file, using fprintf. The command UZC will read
-          % current light levels and store them in a zero correction array. All subsequent light readings have this value subtracted from them before
-          % being returned to the host. Note the '13' represents the terminator character. 13 represents a carriage return and should be included at
-          % the end of every command to indicate when a command is finished.
-          fprintf(obj.rscom, ['UZC',13]);
+          calibrate_ok=0;
+          while ~calibrate_ok
 
-          % This command returns a blank character at the start of each line by default that can
-          % confuse efforts to read the values. Therefore use fscanf once to remove this character.
-          fscanf(obj.rscom);
+            % Commands are passed to the ColorCAL II as though they were being written to a text file, using fprintf. The command UZC will read
+            % current light levels and store them in a zero correction array. All subsequent light readings have this value subtracted from them before
+            % being returned to the host. Note the '13' represents the terminator character. 13 represents a carriage return and should be included at
+            % the end of every command to indicate when a command is finished.
+            fprintf(obj.rscom, ['UZC',13]);
 
-          % To read the returned data, use fscanf, as though reading from a text file
-          dataLine = fscanf(obj.rscom);
+            % This command returns a blank character at the start of each line by default that can
+            % confuse efforts to read the values. Therefore use fscanf once to remove this character.
+            fscanf(obj.rscom);
 
-          % The expected returned messag if successful is 'OKOO' or if an error, 'ER11'. In case of any additional blank characters either side of
-          % these messages, search through each character until either an O or an E is found so that the start of the relevant message can be determined.
-          for ii=1:1:length(dataLine)
+            % To read the returned data, use fscanf, as though reading from a text file
+            dataLine = fscanf(obj.rscom);
 
-            % Once either an O or an E is found, the start of the relevant information is the current character
-            % positiong while the end is 3 characters further (as each possible message is 4 characters in total).
-            if dataLine(kk)=='O' || dataLine(kk)=='E'
-              sp=kk;
-              ep=kk+3;
+            % The expected returned messag if successful is 'OKOO' or if an error, 'ER11'. In case of any additional blank characters either side of
+            % these messages, search through each character until either an O or an E is found so that the start of the relevant message can be determined.
+            for ii=1:1:length(dataLine)
+
+              % Once either an O or an E is found, the start of the relevant information is the current character
+              % positiong while the end is 3 characters further (as each possible message is 4 characters in total).
+              if dataLine(kk)=='O' || dataLine(kk)=='E'
+                sp=kk;
+                ep=kk+3;
+              end
             end
-          end
 
-          % the returned message is the characters between the start and end positions
-          data_quality=dataLine(sp:ep);
+            % the returned message is the characters between the start and end positions
+            data_quality=dataLine(sp:ep);
 
-          % if the message is 'OK00', then report a successful calibration.
-          if strcmp(data_quality,'OK00')
-            % calibration is successful. Changing calibrateSuccess to 1 will break the while loop and allow the script to continue.
-            disp('Zero-calibration successful');
-            calibrate_ok=1;
-          else
-            % display warning message.
-            warning('ERROR during zero-calibration. Perhaps too much residual light. Ensure sensor is covered');
-          end
-
-        end % while ~calibrate_ok
-
-        % ========================================
-        % initialization step 2: get the correction matrix for converting the measured values to XYZ tristimulus values
-        % ========================================
-
-        for ii=1:1:3 % cycle through the 3 rows of the correction matrix.
-          % whichColumn is to indicate the column the current value is to be written to.
-          whichColumn=1;
-
-          % Commands are passed to the ColorCAL II as though they were being written to a text file, using fprintf. The commands 'r01', 'r02'
-          % and 'r03' will return the 1st, 2nd and 3rd rows of the correction matrix respectively. Note the '13' represents the terminator
-          % character. 13 represents a carriage return and should be included at the end of every command to indicate when a command is finished.
-          fprintf(obj.rscom,['r0',num2str(ii),13]);
-
-          % This command returns a blank character at the start of each line by default that can confuse efforts to read the values. Therefore
-          % use fscanf once to remove this character.
-          fscanf(obj.rscom);
-
-          % To read the returned data, use fscanf, as though reading from a text file.
-          dataLine=fscanf(obj.rscom);
-
-          % The returned dataLine will be returned as a string of characters in the form of 'OK00, 8053,52040,50642'. Therefore loop through
-          % each character until a O is found to be sure of the start position of the data.
-          for jj=1:1:length(dataLine)
-
-            % Once an O has been found, assign the start position of the numbers to 5 characters beyond this (i.e. skipping the 'OKOO,').
-            if dataLine(jj) == 'O'
-              sp=jj+5;
-              % A comma (,) indicates the start of a value. Therefore if this is found, the value is the number formed of the next 5 characters.
-            elseif dataLine(jj) == ','
-              ep=jj+5;
-
-              % Using j to indicate the row position and whichColumn to indicate the column position, convert the 5 characters to
-              % a number and assign it to the relevant position.
-              obj.corrmatrix(ii,whichColumn)=str2num(dataLine(sp:ep)); %#ok
-
-              % reset myStart to k+6 (the first value of the next number)
-              sp=jj+6;
-
-              % Add 1 to the whichColumn value so that the next value will be saved to the correct location.
-              whichColumn=whichColumn+1;
+            % if the message is 'OK00', then report a successful calibration.
+            if strcmp(data_quality,'OK00')
+              % calibration is successful. Changing calibrateSuccess to 1 will break the while loop and allow the script to continue.
+              disp('Zero-calibration successful');
+              calibrate_ok=1;
+            else
+              % display warning message.
+              warning('ERROR during zero-calibration. Perhaps too much residual light. Ensure sensor is covered');
             end
-          end % for jj=1:1:length(dataLine)
-        end % for ii = 1:1:3
 
-        % Values returned by the ColorCAL II are 10000 times larger than their actual value. Also, negative values have a further 50000 added to them.
-        % These transformations need to be reversed to get the actual values. The positions of myCorrectionMatrix with values greater than 50000 have
-        % 50000 subtracted from them and then converted to their equivalent negative value.
-        obj.corrmatrix(obj.corrmatrix>50000)=0-(obj.corrmatrix(obj.corrmatrix>50000)-50000);
+          end % while ~calibrate_ok
 
-        % All values are then divided by 10000 to give actual values.
-        obj.corrmatrix=obj.corrmatrix./10000;
-        check=0;
+          % ========================================
+          % initialization step 2: get the correction matrix for converting the measured values to XYZ tristimulus values
+          % ========================================
+
+          for ii=1:1:3 % cycle through the 3 rows of the correction matrix.
+                       % whichColumn is to indicate the column the current value is to be written to.
+            whichColumn=1;
+
+            % Commands are passed to the ColorCAL II as though they were being written to a text file, using fprintf. The commands 'r01', 'r02'
+            % and 'r03' will return the 1st, 2nd and 3rd rows of the correction matrix respectively. Note the '13' represents the terminator
+            % character. 13 represents a carriage return and should be included at the end of every command to indicate when a command is finished.
+            fprintf(obj.rscom,['r0',num2str(ii),13]);
+
+            % This command returns a blank character at the start of each line by default that can confuse efforts to read the values. Therefore
+            % use fscanf once to remove this character.
+            fscanf(obj.rscom);
+
+            % To read the returned data, use fscanf, as though reading from a text file.
+            dataLine=fscanf(obj.rscom);
+
+            % The returned dataLine will be returned as a string of characters in the form of 'OK00, 8053,52040,50642'. Therefore loop through
+            % each character until a O is found to be sure of the start position of the data.
+            for jj=1:1:length(dataLine)
+              % Once an O has been found, assign the start position of the numbers to 5 characters beyond this (i.e. skipping the 'OKOO,').
+              if dataLine(jj) == 'O'
+                sp=jj+5;
+                % A comma (,) indicates the start of a value. Therefore if this is found, the value is the number formed of the next 5 characters.
+              elseif dataLine(jj) == ','
+                ep=jj+5;
+
+                % Using j to indicate the row position and whichColumn to indicate the column position, convert the 5 characters to
+                % a number and assign it to the relevant position.
+                obj.corrmatrix(ii,whichColumn)=str2num(dataLine(sp:ep)); %#ok
+
+                % reset myStart to k+6 (the first value of the next number)
+                sp=jj+6;
+
+                % Add 1 to the whichColumn value so that the next value will be saved to the correct location.
+                whichColumn=whichColumn+1;
+              end
+            end % for jj=1:1:length(dataLine)
+          end % for ii = 1:1:3
+
+          % Values returned by the ColorCAL II are 10000 times larger than their actual value. Also, negative values have a further 50000 added to them.
+          % These transformations need to be reversed to get the actual values. The positions of myCorrectionMatrix with values greater than 50000 have
+          % 50000 subtracted from them and then converted to their equivalent negative value.
+          obj.corrmatrix(obj.corrmatrix>50000)=0-(obj.corrmatrix(obj.corrmatrix>50000)-50000);
+
+          % All values are then divided by 10000 to give actual values.
+          obj.corrmatrix=obj.corrmatrix./10000;
+          check=0;
+
+        catch
+          warning('Initialization error. check device connection and try again');
+          check=1;
+        end % try
       end % if obj.init_flg~=1
     end
 
